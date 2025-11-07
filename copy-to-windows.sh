818bce7c3625624e9c9b01e16628496028761b36
Adrian Liu <adrianliu95@protonmail.com>
2025-11-07 12:18:43 +0000

    create a config folder fo r organization

--- copy-to-windows.sh
+++ copy-to-windows.sh
@@ -3,8 +3,8 @@
 WINDOWS_USER="/mnt/c/Users/howis"
 
 rm -f "$WINDOWS_USER/komorebi.json"
-cp komorebi.json "$WINDOWS_USER/komorebi.json"
+cp config/komorebi.json "$WINDOWS_USER/komorebi.json"
 rm -f "$WINDOWS_USER/.config/whkdrc"
-cp whkdrc "$WINDOWS_USER/.config/whkdrc"
+cp config/whkdrc "$WINDOWS_USER/.config/whkdrc"
 
 echo "Windows dotfiles copied successfully"
