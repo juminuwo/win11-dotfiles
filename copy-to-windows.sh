9f69f2518ef46055b60e18c2aabcc422a43f4814
Adrian Liu <adrianliu95@protonmail.com>
2025-11-12 11:36:40 +0000

    add yasb

--- copy-to-windows.sh
+++ copy-to-windows.sh
@@ -6,5 +6,8 @@ rm -f "$WINDOWS_USER/komorebi.json"
 cp config/komorebi.json "$WINDOWS_USER/komorebi.json"
 rm -f "$WINDOWS_USER/.config/whkdrc"
 cp config/whkdrc "$WINDOWS_USER/.config/whkdrc"
+rm -f "$WINDOWS_USER/.config/yasb"
+cp config/yasb "$WINDOWS_USER/.config/yasb"
 
 echo "Windows dotfiles copied successfully"
+
